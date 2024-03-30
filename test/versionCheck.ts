e411da9206f9e791d4ac2bb27f327a7cca3d248b
almic <mick.ashton@flare-esports.net>
2024-03-30 13:24:03 -0600

    Move tests to test folder

--- test/versionCheck.ts
+++ test/versionCheck.ts
@@ -1,10 +1,12 @@
 import semver from 'semver';
 // @ts-ignore
 import { engines } from '../package';
+import { exit } from 'process';
 
 const version: string = engines.node;
 if (!semver.satisfies(process.version, version)) {
     console.error(`Required node version ${version} does not match current version ${process.version}`);
+    exit(1);
 } else {
     console.log(`Process version ${process.version} satisfies required version ${version}!`);
 }
